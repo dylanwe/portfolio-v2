0ba925119d658335f23a046293a23f4d28a6b108
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-12 21:53:18 +0200

    trying to fix styling for cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,17 @@ import CMS from 'netlify-cms-app'
 
 import markdownStyle from './../styles/markdown-body.css'
 
-CMS.registerPreviewStyle(markdownStyle);
+CMS.init()
+var PostPreview = createClass({
+    render: function() {
+      const { entry, getAsset, widgetsFor } = this.props
+      const imagePath = entry.getIn(['data', 'image'])
+      const image = getAsset(imagePath)
+      return h('div', { className: 'markdown-body' });
+    }
+  })
+
+  CMS.registerPreviewTemplate('blog', PostPreview)
+  CMS.registerPreviewTemplate('projects', PostPreview)
+CMS.registerPreviewStyle(markdownStyle.toString(), { raw: true });
+
