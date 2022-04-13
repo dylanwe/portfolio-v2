ac87485d7ab974a007b28df2b9dcadd8028c85c7
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-13 16:44:52 +0200

    made nav hamburger functional and fixed the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
-import * as React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <>
+    <nav className="fixed bg-white top-0 z-50 w-full border-b-2 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 dark:border-gray-700">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
             <Link to="/">
                 <span className="flex items-center">
@@ -13,33 +16,34 @@ const Nav = () => {
                     </span>
                 </span>
             </Link>
-        <div className="flex md:order-2">
+        <div className="flex md:order-2 ">
           <ThemeToggler>
             {({ theme, toggleTheme }) => (
-              <span className="w-[30px]">
-                <span id="goLight" className="hidden dark:block">
-                  <button onClick={() => toggleTheme("light")} className="text-white">
+              <button onClick={() => (theme === "dark") ? toggleTheme("light") : toggleTheme("dark")} className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 transition-colors">
+                <span id="goLight" className="hidden dark:flex items-center">
+                  <button  className="text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="24" fill="currentColor"><path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 2a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-15a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1zm0 16a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1zM1 9h2a1 1 0 1 1 0 2H1a1 1 0 0 1 0-2zm16 0h2a1 1 0 0 1 0 2h-2a1 1 0 0 1 0-2zm.071-6.071a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM5.757 14.243a1 1 0 0 1 0 1.414L4.343 17.07a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM4.343 2.929l1.414 1.414a1 1 0 0 1-1.414 1.414L2.93 4.343A1 1 0 0 1 4.343 2.93zm11.314 11.314l1.414 1.414a1 1 0 0 1-1.414 1.414l-1.414-1.414a1 1 0 1 1 1.414-1.414z"></path></svg>
                   </button>
                 </span>
-                <span id="goDark" className="dark:hidden">
-                  <button onClick={() => toggleTheme("dark")} className="text-gray-800">
+                <span id="goDark" className="dark:hidden flex items-center content-center">
+                  <button className="text-gray-800">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4 -2 24 24" width="24" fill="currentColor"><path d="M2 10c0 4.43 3.478 8 7.742 8 .658 0 1.302-.085 1.922-.248-2.996-2.2-4.896-5.786-4.896-9.752 0-2.09.527-4.095 1.489-5.853C4.699 2.863 2 6.097 2 10zm6.768-2c0 4.632 3.068 8.528 7.232 9.665A9.555 9.555 0 0 1 9.742 20C4.362 20 0 15.523 0 10S4.362 0 9.742 0c.868 0 1.71.117 2.511.335A10.086 10.086 0 0 0 8.768 8z"></path></svg>
                   </button>
                 </span>
-              </span>
+              </button>
             )}
           </ThemeToggler>
           <button
+            onClick={() => setNavIsOpen(!navIsOpen)}
             data-collapse-toggle="mobile-menu-4"
             type="button"
-            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 transition-colors"
             aria-controls="mobile-menu-4"
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
             <svg
-              className="w-6 h-6"
+              className={`${(navIsOpen) ? "hidden" : ""} w-6 h-6`}
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +55,7 @@ const Nav = () => {
               ></path>
             </svg>
             <svg
-              className="hidden w-6 h-6"
+              className={`${(navIsOpen) ? "" : "hidden"} w-6 h-6`}
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -65,8 +69,8 @@ const Nav = () => {
           </button>
         </div>
         <div
-          className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-          id="mobile-menu-4"
+          className={`${(navIsOpen) ? "" : "hidden"} justify-between items-center w-full md:flex md:w-auto md:order-1"
+          id="mobile-menu-4`}
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
@@ -103,6 +107,8 @@ const Nav = () => {
         </div>
       </div>
     </nav>
+    <div className="pt-[61px] md:pt-[49px]"></div>
+    </>
   )
 }
 
