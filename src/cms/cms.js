1300ce43fba6b81f5bd26aba954f0ac21a8d4381
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-15 23:57:24 +0200

    cms test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,23 @@
 import CMS from 'netlify-cms-app';
-import PostPreview from './preview-templates/PostPreview';
+// import PostPreview from './preview-templates/PostPreview';
 import cmsCSS from './../styles/cms.css'
 import codeHighlights from './../styles/one-dark.css'
 
 CMS.registerPreviewStyle(cmsCSS.toString(), { raw: true });
 CMS.registerPreviewStyle(codeHighlights.toString(), { raw: true });
 
-CMS.registerPreviewTemplate('blog', PostPreview)
-CMS.registerPreviewTemplate('projects', PostPreview)
+// CMS.registerPreviewTemplate('blog', PostPreview)
+// CMS.registerPreviewTemplate('projects', PostPreview)
+
+
+var PostPreview = createClass({
+    render: function() {
+      const { entry, getAsset, widgetsFor } = this.props
+      const imagePath = entry.getIn(['data', 'image'])
+      const image = getAsset(imagePath)
+      return h('div', { className: 'prose' });
+    }
+  })
+
+  CMS.registerPreviewTemplate('blog', PostPreview)
+  CMS.registerPreviewTemplate('projects', PostPreview)
