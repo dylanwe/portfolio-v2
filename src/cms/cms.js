9b99d8005024d066c711493e4cfbfc7e5919947b
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-16 00:26:58 +0200

    add bio to cms preview template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,6 @@
 import CMS from 'netlify-cms-app';
-
 import PostPreview from './preview-templates/PostPreview';
-// import './../styles/cms.css'
-// import codeHighlights from './../styles/one-dark.css'
+
 CMS.init();
 
 CMS.registerPreviewStyle('./../styles/cms.css', { raw: true });
@@ -10,16 +8,3 @@ CMS.registerPreviewStyle('./../styles/one-dark.css', { raw: true });
 
 CMS.registerPreviewTemplate('blog', PostPreview)
 CMS.registerPreviewTemplate('projects', PostPreview)
-
-
-// var PostPreview = createClass({
-//     render: function() {
-//       const { entry, getAsset, widgetsFor } = this.props
-//       const imagePath = entry.getIn(['data', 'image'])
-//       const image = getAsset(imagePath)
-//       return h('div', { className: 'prose' });
-//     }
-//   })
-
-//   CMS.registerPreviewTemplate('blog', PostPreview)
-//   CMS.registerPreviewTemplate('projects', PostPreview)
--- src/cms/preview-templates/PostPreview.jsx
+++ src/cms/preview-templates/PostPreview.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
- import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
+import Bio from "../../components/Bio"
 
- const PostPreview = ({ entry, widgetFor }) => {
-   const tags = entry.getIn(['data', 'tags'])
-   return (
-     <div className='prose'>
-         {widgetFor('body')}
-     </div>
-   )
- }
+const PostPreview = ({ entry, widgetFor }) => {
+  const tags = entry.getIn(["data", "tags"])
+  return (
+    <>
+      <article
+        className="prose dark:prose-invert"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header className="text-center">
+          <p className="mb-2 text-gray-500 dark:text-gray-400">{date}</p>
+          <h1>{title}</h1>
+        </header>
+        <hr />
+        <section dangerouslySetInnerHTML={body} itemProp="articleBody" />
+        <hr />
+      </article>
+      <Bio />
+    </>
+  )
+}
 
- PostPreview.propTypes = {
-   entry: PropTypes.shape({
-     getIn: PropTypes.func,
-   }),
-   widgetFor: PropTypes.func,
- }
+PostPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
 
- export default PostPreview 
+export default PostPreview
