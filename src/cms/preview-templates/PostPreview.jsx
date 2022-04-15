13522c149702095c2ade081eb17cede3c7785fff
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-15 23:40:22 +0200

    cms template test

--- src/cms/preview-templates/PostPreview.jsx
+++ src/cms/preview-templates/PostPreview.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+ import PropTypes from 'prop-types'
 
-const PostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
-  return (
-    <article
-        className="prose dark:prose-invert"
-        itemScope
-        itemType="http://schema.org/Article"
-        >
-        <header className="text-center">
-          <p className="mb-2 text-gray-500 dark:text-gray-400">{widgetFor('date')}</p>
-          <h1>{widgetFor('title')}</h1>
-        </header>
-        <hr />
-        <section>{widgetFor('body')}</section>
-        <hr />
-      </article>
-  )
-}
+ const PostPreview = ({ entry, widgetFor }) => {
+   const tags = entry.getIn(['data', 'tags'])
+   return (
+     <div className='prose'>
+         {widgetFor('body')}
+     </div>
+   )
+ }
 
-PostPreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
+ PostPreview.propTypes = {
+   entry: PropTypes.shape({
+     getIn: PropTypes.func,
+   }),
+   widgetFor: PropTypes.func,
+ }
 
-export default PostPreview
+ export default PostPreview 
