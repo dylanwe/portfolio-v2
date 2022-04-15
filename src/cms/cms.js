795c77045b0f2b43312d9c64574bc0876bc5c87c
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2022-04-16 00:18:36 +0200

    cms test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,25 +1,25 @@
 import CMS from 'netlify-cms-app';
-// import PostPreview from './preview-templates/PostPreview';
+
+import PostPreview from './preview-templates/PostPreview';
 // import './../styles/cms.css'
 // import codeHighlights from './../styles/one-dark.css'
-
 CMS.init();
 
 CMS.registerPreviewStyle('./../styles/cms.css', { raw: true });
 CMS.registerPreviewStyle('./../styles/one-dark.css', { raw: true });
 
-// CMS.registerPreviewTemplate('blog', PostPreview)
-// CMS.registerPreviewTemplate('projects', PostPreview)
+CMS.registerPreviewTemplate('blog', PostPreview)
+CMS.registerPreviewTemplate('projects', PostPreview)
 
 
-var PostPreview = createClass({
-    render: function() {
-      const { entry, getAsset, widgetsFor } = this.props
-      const imagePath = entry.getIn(['data', 'image'])
-      const image = getAsset(imagePath)
-      return h('div', { className: 'prose' });
-    }
-  })
+// var PostPreview = createClass({
+//     render: function() {
+//       const { entry, getAsset, widgetsFor } = this.props
+//       const imagePath = entry.getIn(['data', 'image'])
+//       const image = getAsset(imagePath)
+//       return h('div', { className: 'prose' });
+//     }
+//   })
 
-  CMS.registerPreviewTemplate('blog', PostPreview)
-  CMS.registerPreviewTemplate('projects', PostPreview)
+//   CMS.registerPreviewTemplate('blog', PostPreview)
+//   CMS.registerPreviewTemplate('projects', PostPreview)
